1aa6ec3745768c0a7dd2867292e58d7ed793a4e2
said-louham <111187783+said-louham@users.noreply.github.com>
2023-12-08 20:52:18 +0000

    ref routes

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'welcome home')->name('home');
+Route::get('/', fn () => abort(404))->name('home');
 Route::get('/login', fn () => abort(404))->name('login');
